72a1295097f91a5991d1cc262e60bbdae40a97a6
Malika <malikaoubilla@gmail.com>
2024-05-07 02:09:16 -0700

    0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+0-async_generator.py
+"""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Loop 10 times, wait 1 sec each time"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
