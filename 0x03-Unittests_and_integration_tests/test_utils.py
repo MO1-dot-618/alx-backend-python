33cea6089863185289ff4153706342f5c2d21958
Malika <malikaoubilla@gmail.com>
2024-05-27 13:44:56 +0100

    annotation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """Defines unittests for utils.py."""
 import unittest
 from parameterized import parameterized
+from typing import Dict, Tuple, Union
 from utils import access_nested_map
 
 
@@ -13,17 +14,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
         """unit test for output"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple[str],
+                                         exception: Exception) -> None:
         """unit test for raising error"""
-        self.assertRaises(KeyError)
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
